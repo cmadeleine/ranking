ceb2dca831f9502d14ef1cdc8088a664ef1adf54
Madeleine Carter <cmadeleine02@gmail.com>
2023-11-06 20:36:21 -0600

    add comments

--- ranking_btl.py
+++ ranking_btl.py
@@ -16,13 +16,7 @@ from optspace import OptSpace
 # Low Rank Pairwise Ranking: lrpr
 
 
-def logit(p):
-    return math.log2(p / (1 - p))
-
-def logit_inv(p):
-    return math.pow(2, p) / (1 + math.pow(2, p))
-
-# creating synthetic data for P matrix in BTL and Thurstone models, approximating ideal matrix P*
+# creating synthetic data for P matrix, approximating ideal matrix P*
 def make_P(n, e, L, w):
 
     P_btl = np.zeros((n, n))
@@ -47,7 +41,7 @@ def make_P(n, e, L, w):
     df.to_excel('P_btl.xlsx', sheet_name='P_btl')
     return P_btl         
 
-# creating 1D  w vector: value/weight/score of each element
+# creating 1D  w vector: value/weight/score of each element; ranges from 0.5-1
 def make_w(n, delta_k):
     w = [0] * n
     w[0] = 0.5
@@ -58,6 +52,7 @@ def make_w(n, delta_k):
 
 # approximate w with linear program
 def lp_algorithm(P):
+    # vector approximating w: w_btl
 
     m_btl = gp.Model("BTL")
     # x: 1D vector (used to approximate s)
@@ -73,7 +68,7 @@ def lp_algorithm(P):
     link_btl = np.zeros((n, n))
     for i in range(0, n):
         for j in range(0, n):
-            # we assume no probabilities are zero, if btl is zero, so is thu
+            # we assume no probabilities are zero unless they were not compared
             if (P[i][j] == 0):
                 link_btl[i][j] = 0
             else:
@@ -90,6 +85,7 @@ def lp_algorithm(P):
     m_btl.addConstr((x_btl @ eVec.T) - (eVec @ x_btl.T) - z_btl - link_btl <= zeroMat)
 
     # constraints include entire matrix, but we only care when link[i][j] != 0 and i != j; so apply a mask: np.multiply(z, mask)
+    # this way, the optimization problem can ignore zero entries
     m_btl.setObjective((z_btl * mask).sum(), GRB.MINIMIZE)
 
     m_btl.optimize()
@@ -106,9 +102,11 @@ def lp_algorithm(P):
 
 # from a base matrix P_btl, find the pi vector estimating w
 def spec_algorithm(P_btl):
+    # vector approximating w: pi
 
     P_T = P_btl.T
 
+    # d = maximum row sum
     d = 0
     for i in range(0, n):
         if (sum(P_T[i]) > d):
@@ -121,6 +119,7 @@ def spec_algorithm(P_btl):
             if (P_T[i][j] == 0):
                 P_transit[i][j] = 0
             else:
+                # apply link function
                 if (i==j):
                     P_transit[i][j] = 1 - (1/d) * sum(P_T[i])
                 else:
@@ -139,6 +138,7 @@ def spec_algorithm(P_btl):
         if (np.isclose(evals[i], 1)):
             pi = evecs[:, i]
 
+    # flip the sign if negative (should this be happening?)
     if (pi[0] < 0):
         for i in range(0, n):
             pi[i] = -1 * pi[i]
@@ -147,12 +147,13 @@ def spec_algorithm(P_btl):
     return pi
 
 def mle_algorithm(P_btl, w_min, w_max):
+    # vector approximating w: w_t
 
     w_spec = spec_algorithm(P_btl)
 
     w_mle = [0] * n
 
-    # checking 100 values of tau between min and max
+    # checking 100 values of tau between min and max, could increase/decrease for more/less precision
     tau_step = (w_max - w_min) / 100
 
     w_t = w_spec.copy()
@@ -166,6 +167,7 @@ def mle_algorithm(P_btl, w_min, w_max):
             tau_best = float('-inf')
             max_P = float('-inf')
             tau = w_min
+            # compute probability for each possible value for tau
             while (tau <= w_max):
                 curr_P = 0
                 for j in range(0, n):
@@ -188,6 +190,7 @@ def mle_algorithm(P_btl, w_min, w_max):
         # MLE part 2
         modified = False
         for i in range(0, n):
+            # if difference is large enough, pick new (mle-generated) value
             if (abs(w_mle[i] - w_t[i]) > E_t):
                 w_t[i] = w_mle[i]
                 modified = True
@@ -201,6 +204,7 @@ def mle_algorithm(P_btl, w_min, w_max):
     return w_t
 
 def lrpr_algorithm(P):
+    # vector approximating w: sigma
 
     # P with linking function applied
     link = np.zeros((n, n))
@@ -218,10 +222,12 @@ def lrpr_algorithm(P):
     os = OptSpace(2, 5, 0.0001)
     U, S, V = os.solve(link)
 
+    # construct filled-in matrix
     opt_mat = np.matmul(np.matmul(U, S), V.T)
 
     inv_link = np.zeros((n, n))
     
+    # construct matrix according to algorithm
     for i in range(0, n):
         for j in range(0, n):
                 inv_ij = math.pow(2, opt_mat[i][j]) / (1 + math.pow(2, opt_mat[i][j]))
@@ -233,8 +239,8 @@ def lrpr_algorithm(P):
                 else:
                     inv_link[i][j] = 1/2 - min(abs(inv_ij - 1/2), abs(inv_ji - 1/2))
 
+    # copeland ranking: w[i] = number of entries with probability > 0.5
     sigma = [0] * n
-
     for i in range(0, n):
         for j in range(0, n):
             if (inv_link[i][j] > 0.5):
@@ -243,7 +249,6 @@ def lrpr_algorithm(P):
     s_norm = np.asarray(sigma) / sum(sigma)
 
     return s_norm
-
 
 # run a trial with n items, 
 # where e is the probability that any pair of elements will be compared, 
@@ -255,26 +260,22 @@ def simulate(n, L, e, gap):
 
     #==================Linear Program==================
     # models: btl, thurstone
-    # approximating w vector with: w_btl, w_thu
     print("before lp")
     w_lp = lp_algorithm(P)
     # w_lp = [0]*n
     #==================Spectral Method==================
     # models: btl
-    # approximating w vector with: pi
     print("before spec")
-    pi = spec_algorithm(P)
+    w_spec = spec_algorithm(P)
 
     #======================MLE==========================
     # models: btl
-    # approximating w vector with: w_mle
     print("before mle")
     w_mle = mle_algorithm(P, min(w_norm), max(w_norm))
     # w_mle = [0]*n
 
     #================Low Rank Pairwise Ranking=================
     # models: btl, thu
-    # approximating w vector with: w_lrpr
     print("before lrpr")
     w_lrpr = lrpr_algorithm(P) 
     # w_lrpr = [0]*n
@@ -289,13 +290,14 @@ def simulate(n, L, e, gap):
         w_comp[i][0] = i
         w_comp[i][1] = w_norm[i]
         w_comp[i][2] = w_lp[i]
-        w_comp[i][3] = pi[i]
+        w_comp[i][3] = w_spec[i]
         w_comp[i][4] = w_mle[i]
         w_comp[i][5] = w_lrpr[i]
 
+    # sort each ranking
     w_sort = np.argsort(w)
     lp_sort = np.argsort(w_lp)
-    pi_sort = np.argsort(pi)
+    spec_sort = np.argsort(w_spec)
     mle_sort = np.argsort(w_mle)
     lrpr_sort = np.argsort(w_lrpr)
 
@@ -304,7 +306,7 @@ def simulate(n, L, e, gap):
     for i in range(0, n):
         rankings_comp[i][0] = w_sort[i]
         rankings_comp[i][1] = lp_sort[i]
-        rankings_comp[i][2] = pi_sort[i]
+        rankings_comp[i][2] = spec_sort[i]
         rankings_comp[i][3] = mle_sort[i]
         rankings_comp[i][4] = lrpr_sort[i]
 
@@ -317,20 +319,20 @@ def simulate(n, L, e, gap):
     # l infinity error
     
     l_inf_err[0] = max(abs(np.subtract(w_lp, w_norm)))/max(w_norm)
-    l_inf_err[1] = max(abs(np.subtract(pi, w_norm)))/max(w_norm)
+    l_inf_err[1] = max(abs(np.subtract(w_spec, w_norm)))/max(w_norm)
     l_inf_err[2] = max(abs(np.subtract(w_mle, w_norm)))/max(w_norm)
     l_inf_err[3] = max(abs(np.subtract(w_lrpr, w_norm)))/max(w_norm)
 
     # D_w error
     for i in range(0, n):
         for j in range(i, n):
-            if ((w_norm[i] - w_norm[j]) * (w_lp[i] - w_lp[j]) > 0):
+            if ((w_norm[i] - w_norm[j]) * (w_lp[i] - w_lp[j]) < 0):
                 D_w_err[0] += math.pow((w_norm[i] - w_norm[j]), 2)
-            if ((w_norm[i] - w_norm[j]) * (pi[i] - pi[j]) > 0):
+            if ((w_norm[i] - w_norm[j]) * (w_spec[i] - w_spec[j]) < 0):
                 D_w_err[1] += math.pow((w_norm[i] - w_norm[j]), 2)
-            if ((w_norm[i] - w_norm[j]) * (w_mle[i] - w_mle[j]) > 0):
+            if ((w_norm[i] - w_norm[j]) * (w_mle[i] - w_mle[j]) < 0):
                 D_w_err[2] += math.pow((w_norm[i] - w_norm[j]), 2)
-            if ((w_norm[i] - w_norm[j]) * (w_lrpr[i] - w_lrpr[j]) > 0):
+            if ((w_norm[i] - w_norm[j]) * (w_lrpr[i] - w_lrpr[j]) < 0):
                 D_w_err[3] += math.pow((w_norm[i] - w_norm[j]), 2)
 
     D_w_err[0] = math.sqrt(D_w_err[0] / (2*n*math.pow(np.linalg.norm(w), 2)))
@@ -338,16 +340,16 @@ def simulate(n, L, e, gap):
     D_w_err[2] = math.sqrt(D_w_err[2] / (2*n*math.pow(np.linalg.norm(w), 2)))
     D_w_err[3] = math.sqrt(D_w_err[3] / (2*n*math.pow(np.linalg.norm(w), 2)))
 
-    # alternative error according to LRPR paper
+    # alternative D_w error according to LRPR paper
     # for i in range(0, n):
     #     for j in range(i, n):
-    #         if ((w_norm[i] - w_norm[j]) * (w_lp[i] - w_lp[j]) > 0):
+    #         if ((w_norm[i] - w_norm[j]) * (w_lp[i] - w_lp[j]) < 0):
     #             D_w_err[0] += 1
-    #         if ((w_norm[i] - w_norm[j]) * (pi[i] - pi[j]) > 0):
+    #         if ((w_norm[i] - w_norm[j]) * (w_spec[i] - w_spec[j]) < 0):
     #             D_w_err[1] += 1
-    #         if ((w_norm[i] - w_norm[j]) * (w_mle[i] - w_mle[j]) > 0):
+    #         if ((w_norm[i] - w_norm[j]) * (w_mle[i] - w_mle[j]) < 0):
     #             D_w_err[2] += 1
-    #         if ((w_norm[i] - w_norm[j]) * (w_lrpr[i] - w_lrpr[j]) > 0):
+    #         if ((w_norm[i] - w_norm[j]) * (w_lrpr[i] - w_lrpr[j]) < 0):
     #             D_w_err[3] += 1
 
     # D_w_err[0] = D_w_err[0] / (n * (n-1) / 2)
@@ -357,13 +359,10 @@ def simulate(n, L, e, gap):
 
     return rankings_comp, l_inf_err, D_w_err
 
-
 # running an instance of a simulation, and recording the results in csv files
 def run_trial(n, L, e, gap, rank, err):
 
     rankings, l_inf_err, D_w_err = simulate(n, L, e, gap)
-    # print("rankings: ", rankings)
-    # print("errors: ", errors)
 
     str_lp = "\"btl\",\"lp\"," + str(n) + "," + str(e) + "," + str(L) + "," + str(gap) + ","
     str_spec = "\"btl\",\"spec\"," + str(n) + "," + str(e) + "," + str(L) + "," + str(gap) + ","
@@ -382,6 +381,7 @@ def run_trial(n, L, e, gap, rank, err):
     rank_mle = set()
     rank_lrpr = set()
 
+    # record up to top 20 ranks
     for k in range(0, 20):
         rank_true.add(rankings[k][0])
         rank_lp.add(rankings[k][1])
@@ -394,6 +394,7 @@ def run_trial(n, L, e, gap, rank, err):
         mle_correct = len(rank_true.intersection(rank_mle))
         lrpr_correct = len(rank_true.intersection(rank_lrpr))
 
+        # record when it guesses the set of top k items correctly
         lp_correct = lp_correct == k + 1
         spec_correct = spec_correct == k + 1
         mle_correct = mle_correct == k + 1
@@ -419,7 +420,6 @@ def run_trial(n, L, e, gap, rank, err):
     rank.write(str_spec)
     rank.write(str_mle)
     rank.write(str_lrpr)
-   
 
 def init_csv(main, error) :
     main.write("\"model\",")
@@ -447,13 +447,20 @@ def init_csv(main, error) :
 
 model = 'btl'
 
-ranks = open(("rank_" + model + ".csv"), "w")
-err = open(("error_" + model + ".csv"), "w")
+# if appending trials to an existing file, comment out init_csv and change "w"s to "a"
+ranks = open(("rank_" + model + "8.csv"), "w")
+err = open(("error_" + model + "8.csv"), "w")
 init_csv(ranks, err)
 
-for n in [100]:
-    for L in [10, 30, 50]:  
-        for e in [0.2, 0.5, 0.8]: 
+# n: number of items
+# L: number of comparisons
+# e probability of 2 items being compared
+# gap: imposed gap in w score between first element and all others
+# j: trials per data point
+
+for n in [500]:
+    for L in [10, 20, 30, 40]:  
+        for e in [0.15, 0.30, 0.45]: 
             gap = 0.0
             for j in range(0, 20):
                     print(n, L, e, j)
